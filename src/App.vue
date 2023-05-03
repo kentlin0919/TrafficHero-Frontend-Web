0d2cbb83b421a9ee576c76204e66a36b7e79e0e5
Eddie <zxc1435693@gmail.com>
2023-05-03 23:40:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <header class="sticky-top">
+    <navbar></navbar>
+  </header>
+  <main>
+    <RouterView></RouterView>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  width: 100%;
+  position: fixed;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  left: 0%;
+}
+
+main {
+  right: 0%;
+  left: 0%;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hero</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li>
+          <RouterLink class="nav-link active" aria-current="page" to="/"> Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink class="nav-link active" aria-current="page" to="/about"> about</RouterLink>
+        </li>
+        <li>
+          <RouterLink class="nav-link active" aria-current="page" to="/new"> new</RouterLink>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+</template>
+
+
+
+<style  scoped>
+
+</style>
